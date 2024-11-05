82090e487193a0dbbc8e0504b206e757c99e1313
Christian Stamati <c.stamati@we-wear.biz>
2024-11-05 22:49:59 +0100

    updated project

--- src/app/(frontend)/[locale]/page.tsx
+++ src/app/(frontend)/[locale]/page.tsx
@@ -1,22 +1,9 @@
 import React from "react";
-import { Link } from "@/i18n/routing";
 import LocaleSelect from "@/components/locale-select";
-import { getPayloadHMR } from "@payloadcms/next/utilities";
-import config from "@payload-config";
 import { getTranslations } from "next-intl/server";
+import Link from "next/link";
 
-export default async function Home({
-  params,
-}: {
-  params: Promise<{ locale: string }>;
-}) {
-  const payload = await getPayloadHMR({ config });
-  const locale = (await params).locale;
-  const fruits = await payload.find({
-    collection: "fruits",
-    locale: locale as any,
-    fallbackLocale: "en",
-  });
+export default async function Home() {
   const t = await getTranslations("HomePage");
   return (
     <div className="p-4">
@@ -25,9 +12,6 @@ export default async function Home({
       <Link href="/admin">{t("admin")}</Link>
       <div>
         <div className="pb-2 text-xl font-bold">Fruit (Api)</div>
-        {fruits.docs.map((doc, index) => (
-          <div key={index}>{doc.name}</div>
-        ))}
       </div>
     </div>
   );

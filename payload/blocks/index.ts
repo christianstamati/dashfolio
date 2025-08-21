b332050d32099040e4bcf2023e8deab2d577c21e
Christian Stamati <christian.stamati@gmail.com>
2025-08-21 12:37:27 +0200

    fixed build issue

--- payload/blocks/index.ts
+++ payload/blocks/index.ts
@@ -1,5 +1,3 @@
 import { example } from "./example/config";
-import { image } from "./image/config";
-import { paragraph } from "./paragraph/config";
 
-export const blockConfigs = [image, example, paragraph];
+export const blockConfigs = [example];

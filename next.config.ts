4b52789a14c83cb128232ab7df75a8b22fa75516
Christian Stamati <christian.stamati@gmail.com>
2025-09-10 22:43:42 +0200

    update next config and generated stuff

--- next.config.ts
+++ next.config.ts
@@ -15,18 +15,10 @@ const nextConfig: NextConfig = {
 				hostname: "dashfolio-prod.s3.eu-central-1.amazonaws.com",
 			},
 		],
-		formats: ["image/webp", "image/avif"],
-		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-		minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
 	},
 	compress: true,
 	poweredByHeader: false,
 	generateEtags: true,
-	experimental: {
-		scrollRestoration: true,
-		inlineCss: true,
-	},
 	compiler: {
 		removeConsole: process.env.NODE_ENV === "production",
 	},

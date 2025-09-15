8e2be923c1c632300a4f84ee29e9de868bdcdfbd
Christian Stamati <christian.stamati@gmail.com>
2025-09-15 22:53:53 +0200

    disable dynamic params

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,4 +1,5 @@
 import PageTemplate, { generateMetadata } from "./[slug]/page";
 export const dynamic = "force-static";
+export const dynamicParams = false;
 export default PageTemplate;
 export { generateMetadata };

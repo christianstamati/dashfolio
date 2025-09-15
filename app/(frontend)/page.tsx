27245af3ac4ae46a6654b213627a78307a5876cb
Christian Stamati <christian.stamati@gmail.com>
2025-09-15 21:04:25 +0200

    added generate static site params

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,7 +1,29 @@
+import { getPayloadClient } from "@/payload/client";
 import PageTemplate, { generateMetadata } from "./[slug]/page";
 export default PageTemplate;
 export { generateMetadata };
 
-// favicon
-// add password for projects
-// add content
+export async function generateStaticParams() {
+	const payload = await getPayloadClient();
+	const pages = await payload.find({
+		collection: "pages",
+		draft: false,
+		limit: 1000,
+		overrideAccess: false,
+		pagination: false,
+		select: {
+			slug: true,
+		},
+		where: {
+			slug: {
+				equals: "home",
+			},
+		},
+	});
+
+	const params = pages.docs.map(({ slug }) => {
+		return { slug };
+	});
+
+	return params;
+}
